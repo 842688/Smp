58b4f9569312a1023860aabacc9443f93fe8a477
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-08 17:19:30 +0100

    Create MaterialsScreen.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/fobicx/screens/MaterialsScreen.kt
@@ -0,0 +1,12 @@
+package com.gibson.fobicx.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MaterialsScreen{
+    Text("Materials", modifier = Modifier.padding(16.dp))
+}
