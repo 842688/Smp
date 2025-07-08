4c6a8339280afabca6e91dc8f0019d8492c1342f
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-08 16:47:29 +0100

    Create Color.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/fobicx/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.gibson.fobicx.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val DarkBackground = Color(0xFF1C1C1E) // Matches dark bottom bar in your screenshot
+val DarkSurfaceVariant = Color(0xFF2C2C2E) // Slightly lighter for nav bar container
+
+val PrimaryColor = Color(0xFF3B82F6) // Blue (customizable)
+val OnPrimary = Color.White
+val OnSurface = Color(0xFFDDDDDD)
