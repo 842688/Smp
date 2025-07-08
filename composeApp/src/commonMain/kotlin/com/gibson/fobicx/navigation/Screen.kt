de8011b64316d6c897241866e16cdd51f053ead5
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-08 21:37:21 +0100

    Update Screen.kt

--- composeApp/src/commonMain/kotlin/com/gibson/fobicx/navigation/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/fobicx/navigation/Screen.kt
@@ -1,9 +1,13 @@
 package com.gibson.fobicx.navigation
 
-sealed class Screen(val route: String ) {
-    object Home : Screen("Home")
-    object Materials : Screen("Materials")
-    object Post : Screen("Post")
-    object Stock : Screen("Stock")
-    object Me : Screen("Me")
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Materials : Screen("market")
+    object Post : Screen("post")
+    object Stock : Screen("stock")
+    object Me : Screen("me")
+
+    companion object {
+        val allScreens = listOf(Home, Materials, Post, Stock, Me)
+    }
 }
