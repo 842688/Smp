4b3857fb9b821bcc7d7942edd99f64c0282e45ff
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-08 17:23:16 +0100

    Create StockScreen.kt

---
+++ composeApp/src/commonMain/kotlin/com/gibson/fobicx/screens/StockScreen.kt
@@ -0,0 +1,12 @@
+package com.gibson.fobicx.ui.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun StockScreen() {
+    Text("Stocks", modifier = Modifier.padding(16.dp))
+}
