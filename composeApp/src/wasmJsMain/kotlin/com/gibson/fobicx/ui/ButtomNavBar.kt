9ceec5fd45d862ddfa306a3e977e8f15759e0553
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-09 12:58:01 +0100

    Create ButtomNavBar.kt

---
+++ composeApp/src/wasmJsMain/kotlin/com/gibson/fobicx/ui/ButtomNavBar.kt
@@ -0,0 +1,12 @@
+package com.gibson.fobicx.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+actual fun BottomNavBar(
+    currentRoute: String?,
+    onItemClick: (String) -> Unit
+) {
+    Text(" web app Bottom Navigation Coming Soon")
+}
