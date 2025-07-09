2c6bf2b0e6947a8a874848ebc61240276f0f3716
user <gibsonezeh6@gmail.com>
2025-07-09 14:31:32 +0000

    smp

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -4,17 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import com.gibson.fobicx.ui.theme.FobicxTheme
 import androidx.compose.ui.tooling.preview.Preview
+import com.gibson.fobicx.ui.BottomNavBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
-            FobicxTheme{
-            AppAndroid()
+           FobicxTheme{
+           AppAndroid()
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/gibson/fobicx/ui/BottomNavBar.kt
+++
@@ -1,9 +0,0 @@
-package com.gibson.fobicx.ui
-
-import androidx.compose.runtime.Composable
-
-@Composable
-expect fun BottomNavBar(
-    currentRoute: String?,
-    onItemClick: (String) -> Unit
-)
