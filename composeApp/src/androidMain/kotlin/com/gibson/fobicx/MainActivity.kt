b24bfa31442011abae285edd48b16a839a7c2e2a
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-09 16:08:19 +0100

    Update MainActivity.kt

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/MainActivity.kt
@@ -3,21 +3,16 @@ package com.gibson.fobicx
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import com.gibson.fobicx.navigation.AppNavigation
 import com.gibson.fobicx.ui.theme.FobicxTheme
-import androidx.compose.ui.tooling.preview.Preview
-import com.gibson.fobicx.ui.BottomNavBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-
         setContent {
-           FobicxTheme{
-           AppAndroid()
+            FobicxTheme (isDarkTheme = isSystemInDarkTheme()){
+                AppNavigation()
             }
         }
     }
