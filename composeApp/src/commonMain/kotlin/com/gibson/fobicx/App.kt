a9b75fcb825f7d01f28ed7e6f3ddfc989b6c99fa
gibsonezeh <131612941+gibsonezeh@users.noreply.github.com>
2025-07-09 12:08:33 +0100

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/gibson/fobicx/App.kt
+++ composeApp/src/commonMain/kotlin/com/gibson/fobicx/App.kt
@@ -1,47 +1,8 @@
 package com.gibson.fobicx
 
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.ui.unit.dp
-import org.jetbrains.compose.navigation.compose.NavHost
-import org.jetbrains.compose.navigation.compose.composable
-import org.jetbrains.compose.navigation.compose.rememberNavController
-import com.gibson.fobicx.screens.*
-import com.gibson.fobicx.ui.components.BottomNavBar
+import com.gibson.fobicx.navigation.Screen
 
-@Composable
-fun App() {
-    val navController = rememberNavController()
-    val currentRoute by remember {
-        derivedStateOf { navController.currentBackStackEntry?.destination?.route ?: "home" }
-    }
-
-    MaterialTheme {
-        Scaffold(
-            bottomBar = {
-                BottomNavBar(
-                    currentRoute = currentRoute,
-                    onItemClick = { route ->
-                        if (route != currentRoute) {
-                            navController.navigate(route)
-                        }
-                    }
-                )
-            }
-        ) { innerPadding ->
-            NavHost(
-                navController = navController,
-                startDestination = "home",
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                composable("home") { HomeScreen() }
-                composable("market") { MarketScreen() }
-                composable("post") { PostScreen() }
-                composable("stock") { StockScreen() }
-                composable("me") { ProfileScreen() }
-            }
-        }
-    }
+class AppState {
+    var currentScreen by mutableStateOf<Screen>(Screen.Home)
 }
