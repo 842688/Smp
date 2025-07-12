4fbe0cc3813b69a773cde684b2f29096a2b584f1
gibson <gibsonezeh6@gmail.com>
2025-07-12 15:36:47 +0000

    smp

--- composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/AppNavigation.kt
+++ composeApp/src/androidMain/kotlin/com/gibson/fobicx/navigation/AppNavigation.kt
@@ -3,13 +3,11 @@ package com.gibson.fobicx.navigation
 
 import androidx.compose.runtime.Composable 
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController 
-import androidx.navigation.compose.NavHost 
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable 
 import androidx.navigation.compose.rememberNavController 
 import com.gibson.fobicx.ui.screens.* 
 import com.gibson.fobicx.ui.screens.auth.*
-import com.gibson.fobicx.ui.screens.pages.*
 import com.gibson.fobicx.viewmodel.AuthViewModel
 
 object Routes {
